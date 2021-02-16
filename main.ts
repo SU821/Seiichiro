4f00c4d0d09253d5d7362cdba0176f958b287f54
SU821 <79069909+SU821@users.noreply.github.com>
2021-02-16 17:17:44 +0900

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,39 @@
+maqueen.IR_callbackUser(function (message) {
+    basic.showNumber(maqueen.IR_read())
+    if (message == 135) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (message == 134) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else if (message == 156) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else if (message == 149) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else {
+        basic.showIcon(IconNames.Heart)
+    }
+})
+maqueenIR.initIR(Pins.P16)
